a3e56d53f78cea92e01c1080929a7d103fae173f
Brian Ferri <brian.ferri@dotenv.it>
2024-03-04 14:48:24 +0100

    feat: refactor websocket server to support new routing

--- src/router/websocket.ts
+++ src/router/websocket.ts
@@ -1,5 +1,10 @@
-import { Server as BunServer, ServerWebSocket } from 'bun'
+import {
+  Server as BunServer,
+  ServerWebSocket,
+  WebSocketServeOptions
+} from 'bun'
 import { Middleware, runMiddleware } from './middleware'
+import Router from './router'
 
 enum WebSocketEvents {
   message = 'message',
@@ -26,8 +31,7 @@ class WebSocket<TClass = Function> extends WebSocketGenerator {
   constructor(
     public path: string,
     public target: TClass,
-    public handlers: WebSocketHandler[],
-    public port?: number
+    public handlers: WebSocketHandler[]
   ) {
     super()
     this.path = path
@@ -49,25 +53,12 @@ class WebSocket<TClass = Function> extends WebSocketGenerator {
     return this
   }
 
-  public parsePath(path: string) {
-    const requestParts = path.split('/')
-    if (requestParts.length !== this.pathParts.length) return false
-    for (let i = 0; i < this.pathParts.length; i++) {
-      const part = this.pathParts[i]
-      if (!(part.charAt(0) === ':' || part === requestParts[i])) return false
-      if (part.charAt(0) === ':') {
-        this.parameters[part.slice(1)] = requestParts[i]
-      }
-    }
-    return true
-  }
-
   public get params() {
     return this.parameters
   }
 }
 
-export function ws(path: string, port?: number) {
+export function ws(path: string) {
   return <TClass extends new (...args: any[]) => any>(
     target: TClass,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars -- Context is a mandatory parameter but is always undefined in this case
@@ -83,23 +74,20 @@ export function ws(path: string, port?: number) {
         handlers.push(method)
       }
     }
-    return new WebSocket(path, target, handlers, port) as WebSocket<TClass> &
-      TClass // https://github.com/Microsoft/TypeScript/issues/4881
+    return new WebSocket(path, target, handlers) as WebSocket<TClass> & TClass // https://github.com/Microsoft/TypeScript/issues/4881
   }
 }
 
-type WSMap = Record<number, Record<string, WebSocket>>
-
-function webSocketHandlerRouter(routes: Record<string, WebSocket>) {
+function webSocketRouterHandler(routes: Router) {
   const router = {} as WebSocketMethods
   for (const event in WebSocketEvents) {
     router[event as WebSocketEventString] = function (
       socket: ServerWebSocket<any>,
       ...args: any[]
     ) {
-      const route = routes[socket.data.route]
-      if (typeof route === 'undefined') return
-      return route[event as WebSocketEventString](
+      const route = routes.find(socket.data.route)
+      if (route === null) return
+      return route.store[0][event as WebSocketEventString](
         socket,
         ...args,
         socket.data.params
@@ -108,6 +96,10 @@ function webSocketHandlerRouter(routes: Record<string, WebSocket>) {
   }
   return router
 }
+
+type WebSocketServerOptions = {
+  [key in keyof WebSocketServeOptions]?: WebSocketServeOptions[key]
+} & Middleware
 
 export class WebSocketServer<
   TWSRoutes extends Record<string, unknown> = Record<string, WebSocket>
@@ -118,15 +110,21 @@ export class WebSocketServer<
   public websockets: Record<keyof TWSRoutes, WebSocket>
 
   /**
-   * The BunServer instances
+   * The Router
    */
-  public instances: BunServer[] = []
+  private router = new Router()
 
   /**
-   * Map of WebSocket port to WebSocket paths
+   * The BunServer instances
    */
-  public wsMap: WSMap = {}
-  constructor(websockets: TWSRoutes, options?: Middleware) {
+  public instance: BunServer
+
+  private addRoute<T>(path: string, route: T) {
+    const store = this.router.register<Record<number, T>>(path)
+    store[0] = route
+  }
+
+  constructor(websockets: TWSRoutes, options?: WebSocketServerOptions) {
     this.websockets = websockets as Record<keyof TWSRoutes, WebSocket>
     for (const socket of Object.values(this.websockets)) {
       if (!(socket instanceof WebSocket)) {
@@ -134,61 +132,46 @@ export class WebSocketServer<
           `Did you forget to apply the decorator?\nInvalid WebSocket class: \n${socket}`
         )
       }
-      if (socket.port === undefined) socket.port = 443
-      if (!this.wsMap[socket.port]) this.wsMap[socket.port] = {}
-      const path = socket.path
-      if (this.wsMap[socket.port][path]) {
-        throw new Error(
-          `Duplicate WebSocket path: ${path} on port ${socket.port}`
+      this.addRoute(socket.path, socket)
+    }
+    const routeMap = this.router
+    if (
+      options?.fetch !== undefined &&
+      process.env['OVERWRITE_FETCH'] !== 'true'
+    ) {
+      console.warn(
+        "You are overwriting the fetch method. This may cause unexpected behavior. If you are sure you want to do this, set the OVERWRITE_FETCH environment variable to 'true'"
+      )
+      delete options.fetch
+    }
+    this.instance = Bun.serve({
+      async fetch(request, server) {
+        const url = new URL(request.url)
+        const wsRoute = routeMap.find(url.pathname)
+        if (wsRoute === null) {
+          return new Response(`Route not found for ${request.url}`, {
+            status: 404
+          })
+        }
+        if (
+          !server.upgrade(request, {
+            data: { route: wsRoute.store[0].path, params: wsRoute.params }
+          })
+        ) {
+          return new Response('Upgrade failed!', { status: 500 })
+        }
+        const middlewareResponse = runMiddleware(
+          request,
+          options?.middleware,
+          options?.middlewarePaths
         )
-      }
-      this.wsMap[socket.port][path] = socket
-    }
-
-    for (const [port, routes] of Object.entries(this.wsMap)) {
-      const routeArray = Object.values(routes)
-      const instance = Bun.serve({
-        async fetch(request, server) {
-          const url = new URL(request.url)
-          let wsRoute: WebSocket | undefined
-          for (const route of routeArray) {
-            if (route.parsePath(url.pathname)) {
-              wsRoute = route
-              break
-            }
-          }
-          if (typeof wsRoute === 'undefined') {
-            return new Response(`Route not found for ${request.url}`, {
-              status: 404
-            })
-          }
-          const middlewareResponse = runMiddleware(
-            request,
-            options?.middleware,
-            options?.middlewarePaths
-          )
-          if (middlewareResponse instanceof Response) return middlewareResponse
-          if (middlewareResponse instanceof Request) {
-            request = middlewareResponse
-          }
-          if (
-            !server.upgrade(request, {
-              data: { route: wsRoute.path, params: wsRoute.params }
-            })
-          ) {
-            return new Response('Upgrade failed!', { status: 500 })
-          }
-          return new Response(null, { status: 101 })
-        },
-        websocket: webSocketHandlerRouter(routes),
-        port: Number(port)
-      })
-      this.instances.push(instance)
-      delete this.wsMap[Number(port)]
-      const assignedPort = instance.port
-      for (const route of routeArray) route.port = assignedPort
-      this.wsMap[assignedPort] = routes
-    }
+        if (middlewareResponse instanceof Response) return middlewareResponse
+        if (middlewareResponse instanceof Request) request = middlewareResponse
+        return new Response(null, { status: 101 })
+      },
+      websocket: webSocketRouterHandler(routeMap),
+      ...options
+    })
     return this
   }
 }
