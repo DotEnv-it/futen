775ebdf94471836d82c2abd9bfe7b071fcaa2a64
Brian Ferri <brian.ferri@dotenv.it>
2024-03-05 22:38:04 +0100

    refactor: unifies server constructor for WS and REST servers

--- src/router/core.ts
+++ src/router/core.ts
@@ -1,4 +1,6 @@
-import { HTTPRoute } from './rest'
+import { HTTPRoute } from './http'
+import Router from './router'
+import { ServerOptions, WebSocketServerOptions } from './servers'
 import { WSRoute } from './websocket'
 
 /**
@@ -48,5 +50,37 @@ export class Route {
         overrideMethods(this, WSRoute, target.prototype)
         return this
     }
+  }
+}
+
+export abstract class Server<T extends Record<string, unknown>, K> {
+  public routes: Record<keyof T, Route & K>
+  protected router = new Router()
+
+  private addRoute<T>(path: string, route: T) {
+    const store = this.router.register<Record<number, T>>(path)
+    store[0] = route
+  }
+
+  constructor(routes: T, options?: ServerOptions | WebSocketServerOptions) {
+    for (const route of Object.values(routes)) {
+      if (!(route instanceof Route)) {
+        throw new Error(
+          `Did you forget to apply the decorator?\nInvalid route class: \n${route}`
+        )
+      }
+      this.addRoute(route.path, route)
+    }
+    this.routes = routes as Record<keyof T, Route & K>
+    if (
+      options?.fetch !== undefined &&
+      process.env['OVERWRITE_FETCH'] !== 'true'
+    ) {
+      console.warn(
+        "You are overwriting the fetch method. This may cause unexpected behavior. If you are sure you want to do this, set the OVERWRITE_FETCH environment variable to 'true'"
+      )
+      delete options.fetch
+    }
+    return this
   }
 }
--- src/router/websocket.ts
+++ src/router/websocket.ts
@@ -1,11 +1,5 @@
-import {
-  Server as BunServer,
-  ServerWebSocket,
-  WebSocketServeOptions
-} from 'bun'
-import { Middleware, runMiddleware } from './middleware.ts'
+import { ServerWebSocket, WebSocketServeOptions } from 'bun'
 import Router from './router.ts'
-import { Route } from './core.ts'
 
 // type BaseWSHandler<T extends Record<string, unknown> = any> = (ws: BunServerWebSocket, ...args: T[keyof T][]) => void | Promise<void>
 /**
@@ -33,23 +27,7 @@ export const WSRoute = {
   pong: (_ws: ServerWebSocket, _data: Buffer) => {}
 } satisfies WebSocketServeOptions['websocket']
 
-/**
- * Decorator for WebSocket routes
- * ---
- * This decorator is used to define WebSocket routes
- * @param path - The path of the route
- */
-export function ws(path: string) {
-  return function <T extends new (...args: any[]) => any>(
-    target: T,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars -- Context is a mandatory parameter but is always undefined in this case
-    _context: ClassDecoratorContext<T>
-  ) {
-    return new Route(target, path, 'ws') as Route & typeof WSRoute & T
-  }
-}
-
-function webSocketRouterHandler(routes: Router) {
+export function webSocketRouterHandler(routes: Router) {
   const router = {} as typeof WSRoute
   for (const event in WSRoute) {
     router[event as keyof typeof WSRoute] = function (
@@ -66,81 +44,4 @@ function webSocketRouterHandler(routes: Router) {
     }
   }
   return router
-}
-
-type WebSocketServerOptions = {
-  [key in keyof WebSocketServeOptions]?: WebSocketServeOptions[key]
-} & Middleware
-
-export class WebSocketServer<T extends Record<string, unknown>> {
-  /**
-   * The WebSocket instances
-   */
-  public websockets: Record<keyof T, Route & typeof WSRoute>
-
-  /**
-   * The Router
-   */
-  private router = new Router()
-
-  /**
-   * The BunServer instances
-   */
-  public instance: BunServer
-
-  private addRoute<T>(path: string, route: T) {
-    const store = this.router.register<Record<number, T>>(path)
-    store[0] = route
-  }
-
-  constructor(websockets: T, options?: WebSocketServerOptions) {
-    this.websockets = websockets as Record<keyof T, Route & typeof WSRoute>
-    for (const socket of Object.values(this.websockets)) {
-      if (!(socket instanceof Route)) {
-        throw new Error(
-          `Did you forget to apply the decorator?\nInvalid WebSocket class: \n${socket}`
-        )
-      }
-      this.addRoute(socket.path, socket)
-    }
-    const routeMap = this.router
-    if (
-      options?.fetch !== undefined &&
-      process.env['OVERWRITE_FETCH'] !== 'true'
-    ) {
-      console.warn(
-        "You are overwriting the fetch method. This may cause unexpected behavior. If you are sure you want to do this, set the OVERWRITE_FETCH environment variable to 'true'"
-      )
-      delete options.fetch
-    }
-    this.instance = Bun.serve({
-      async fetch(request, server) {
-        const url = new URL(request.url)
-        const wsRoute = routeMap.find(url.pathname)
-        if (wsRoute === null) {
-          return new Response(`Route not found for ${request.url}`, {
-            status: 404
-          })
-        }
-        const middlewareResponse = runMiddleware(
-          request,
-          options?.middleware,
-          options?.middlewarePaths
-        )
-        if (middlewareResponse instanceof Response) return middlewareResponse
-        if (middlewareResponse instanceof Request) request = middlewareResponse
-        if (
-          !server.upgrade(request, {
-            data: { route: wsRoute.store[0].path, params: wsRoute.params }
-          })
-        ) {
-          return new Response('Upgrade failed!', { status: 500 })
-        }
-        return new Response(null, { status: 101 })
-      },
-      websocket: webSocketRouterHandler(routeMap),
-      ...options
-    })
-    return this
-  }
 }
