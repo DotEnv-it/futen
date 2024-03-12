167c15899bcb48314f604131663b47cc83f4f6f7
Brian Ferri <brian.ferri@dotenv.it>
2024-03-12 18:08:25 +0100

    Update package.json and server index.ts, delete types.ts, and add benchmarking for Elysia

--- src/router/types.ts
+++
@@ -1,5 +0,0 @@
-type Segment<T extends string> = T extends `${string}:${infer Param}` ? Param : T extends '*' ? '$' : never;
-export type ParamsKey<Path extends string> = Path extends `${infer Part}/${infer Rest}` ? Segment<Part> | ParamsKey<Rest> : Segment<Path>;
-export type Params<Path extends string, Value = string> = {
-    [K in ParamsKey<Path>]: Value;
-};
