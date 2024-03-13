92d5221a1ebda84233518f6a64bb9c1494c8a6fe
Brian Ferri <brian.ferri@dotenv.it>
2024-03-13 17:12:02 +0100

    Refactor REST test and update tsconfig.json

--- tests/rest.test.ts
+++ tests/rest.test.ts
@@ -1,69 +1,64 @@
-import { describe, test, expect } from 'bun:test'
-import Futen, { route } from '../dist/index.mjs'
+import Futen, { route } from '../dist/index.mjs';
+import { describe, test, expect } from 'bun:test';
 
 describe('REST', () => {
-  @route('/')
-  class Home {
-    get() {
-      const routes = Object.entries(server.routes).map(
-        ([routeClass, route]) => {
-          return {
-            class: routeClass,
-            path: route.path
-          }
+    @route('/')
+    class Home {
+        public get(): Response {
+            // eslint-disable-next-line @typescript-eslint/no-use-before-define
+            const routes = Object.entries(server.routes).map(([routeClass, handler]) => {
+                return {
+                    class: routeClass,
+                    path: handler.path
+                };
+            });
+            return Response.json({
+                routes
+            });
         }
-      )
-      return Response.json({
-        routes
-      })
     }
-  }
 
-  @route('/test')
-  class Test {
-    async post(request: Request) {
-      return Response.json({ object: await request.json() })
+    @route('/test')
+    class Test {
+        public async post(request: Request): Promise<Response> {
+            return Response.json({ object: await request.json() });
+        }
     }
-  }
 
-  const server = new Futen(
-    {
-      Home,
-      Test
-    },
-    {
-      port: 0
-    }
-  )
-
-  const port = server.instance.port
-  test('should return routes', async () => {
-    const response = await server.instance.fetch(
-      new Request(`http://localhost:${port}/`)
-    )
-    const body = await response.json()
-    expect(body).toEqual({
-      routes: [
+    const server = new Futen(
         {
-          class: 'Home',
-          path: '/'
+            Home,
+            Test
         },
         {
-          class: 'Test',
-          path: '/test'
+            port: 0
         }
-      ]
-    })
-  })
+    );
 
-  test('should return request body', async () => {
-    const response = await server.instance.fetch(
-      new Request(`http://localhost:${port}/test`, {
-        method: 'post',
-        body: JSON.stringify({ hello: 'world' })
-      })
-    )
-    const body = await response.json()
-    expect(body).toEqual({ object: { hello: 'world' } })
-  })
-})
+    const { port } = server.instance;
+    test('should return routes', async () => {
+        const response = await server.instance.fetch(new Request(`http://localhost:${port}/`));
+        const body = await response.json();
+        expect(body).toEqual({
+            routes: [
+                {
+                    class: 'Home',
+                    path: '/'
+                },
+                {
+                    class: 'Test',
+                    path: '/test'
+                }
+            ]
+        });
+    });
+
+    test('should return request body', async () => {
+        const response = await server.instance.fetch(new Request(`http://localhost:${port}/test`, {
+            method: 'post',
+            body: JSON.stringify({ hello: 'world' })
+        }));
+        const body = await response.json();
+        expect(body).toEqual({ object: { hello: 'world' } });
+    });
+});
--- tests/websocket.test.ts
+++ tests/websocket.test.ts
@@ -1,123 +1,119 @@
-import { describe, test, expect } from 'bun:test'
-import { ServerWebSocket } from 'bun'
-import Futen, { WebSocketDataType, ws } from '../dist/index.mjs'
+import Futen, { ws } from '../dist/index.mjs';
+import { describe, test, expect } from 'bun:test';
+import type { WebSocketDataType } from '../dist/index.mjs';
+import type { ServerWebSocket } from 'bun';
 
 describe('WebSocket', () => {
-  @ws('/websocket1')
-  class WebSocket1 {
-    message(ws: ServerWebSocket, message: string) {
-      return ws.send(message + ' I received your message')
+    @ws('/websocket1')
+    class WebSocket1 {
+        public message(websocket: ServerWebSocket, message: string): number {
+            return websocket.send(`${message} I received your message`);
+        }
+
+        public close(websocket: ServerWebSocket): void {
+            websocket.close();
+        }
+
+        public open(websocket: ServerWebSocket): number {
+            return websocket.send('Hello, world! from FirstWebSocket');
+        }
     }
 
-    close(ws: ServerWebSocket) {
-      return ws.close()
+    @ws('/websocket2/:id/:id2')
+    class WebSocket2 {
+        public message(
+            websocket: ServerWebSocket<WebSocketDataType>,
+            message: string,
+            params: { id: string, id2: string }
+        ): number {
+            if (message === 'Close me, please!') {
+                return wsServer.routes.WebSocket2.close(
+                    websocket,
+                    1000,
+                    `Goodbye, world! from SecondWebSocket with params ${JSON.stringify(params)}`
+                );
+            }
+            const messageWithParams = `${message} from SecondWebSocket${JSON.stringify(params)}`;
+            return websocket.send(messageWithParams);
+        }
+
+        public close(
+            websocket: ServerWebSocket,
+            code: number,
+            message: string,
+            params: { id: string, id2: string }
+        ): void {
+            expect(code).toBe(1000);
+            websocket.close(
+                1000,
+                `WebSocket closed from SecondWebSocket with code 1000 and message ${message} and params ${JSON.stringify(params)}`
+            );
+        }
+
+        public open(websocket: ServerWebSocket, params: { id: string, id2: string }): number {
+            const openMessage = `WebSocket opened from SecondWebSocket with params ${JSON.stringify(params)}`;
+            return websocket.send(openMessage);
+        }
     }
 
-    open(ws: ServerWebSocket) {
-      return ws.send('Hello, world! from FirstWebSocket')
-    }
-  }
+    const wsServer = new Futen(
+        {
+            WebSocket1,
+            WebSocket2
+        },
+        {
+            port: 0
+        }
+    );
 
-  @ws('/websocket2/:id/:id2')
-  class WebSocket2 {
-    message(
-      ws: ServerWebSocket<WebSocketDataType>,
-      message: string,
-      params: { id: string; id2: string }
-    ) {
-      if (message === 'Close me, please!') {
-        return wsServer.routes.WebSocket2.close(
-          ws,
-          1000,
-          `Goodbye, world! from SecondWebSocket with params ${JSON.stringify(params)}`
-        )
-      }
-      const messageWithParams =
-        message + ' from SecondWebSocket' + JSON.stringify(params)
-      return ws.send(messageWithParams)
-    }
+    const randomPort = wsServer.instance.port;
+    test('should verify the ports of the instances', () => {
+        expect(randomPort).toBeGreaterThan(0);
+    });
 
-    close(
-      ws: ServerWebSocket,
-      code: number,
-      message: string,
-      params: { id: string; id2: string }
-    ) {
-      expect(code).toBe(1000)
-      return ws.close(
-        1000,
-        `WebSocket closed from SecondWebSocket with code 1000 and message ${message} and params ${JSON.stringify(params)}`
-      )
-    }
+    test('should verify the paths of the instances', () => {
+        expect(wsServer.routes.WebSocket1.path).toBe('/websocket1');
+        expect(wsServer.routes.WebSocket2.path).toBe('/websocket2/:id/:id2');
+    });
 
-    open(ws: ServerWebSocket, params: { id: string; id2: string }) {
-      const openMessage = `WebSocket opened from SecondWebSocket with params ${JSON.stringify(params)}`
-      return ws.send(openMessage)
-    }
-  }
+    test('should verify the message event of the instances', async () => {
+        const websocket = new WebSocket(`ws://localhost:${randomPort}/websocket1`);
+        let expectsFired = 0;
+        websocket.onmessage = (event) => {
+            expect(event.data).toBe('Hello, world! from FirstWebSocket');
+            websocket.close();
+            expectsFired++;
+        };
+        websocket.onopen = () => { websocket.send('Hello, world!'); };
+        await new Promise((resolve) => { websocket.onclose = resolve; });
+        expect(expectsFired).toBe(1);
+    });
 
-  const wsServer = new Futen(
-    {
-      WebSocket1,
-      WebSocket2
-    },
-    {
-      port: 0
-    }
-  )
+    test('should verify the message event with params of the instances', async () => {
+        const websocket = new WebSocket(`ws://localhost:${randomPort}/websocket2/1/2`);
+        let expectsFired = 0;
+        websocket.onmessage = (event) => {
+            expect(event.data).toBe('WebSocket opened from SecondWebSocket with params {"id2":"2","id":"1"}');
+            websocket.close();
+            expectsFired++;
+        };
+        websocket.onopen = () => { websocket.send('Hello, world!'); };
+        await new Promise((resolve) => { websocket.onclose = resolve; });
+        expect(expectsFired).toBe(1);
+    });
 
-  const randomPort = wsServer.instance.port
-  test('should verify the ports of the instances', () => {
-    expect(randomPort).toBeGreaterThan(0)
-  })
-
-  test('should verify the paths of the instances', () => {
-    expect(wsServer.routes.WebSocket1.path).toBe('/websocket1')
-    expect(wsServer.routes.WebSocket2.path).toBe('/websocket2/:id/:id2')
-  })
-
-  test('should verify the message event of the instances', async () => {
-    const ws = new WebSocket(`ws://localhost:${randomPort}/websocket1`)
-    let expectsFired = 0
-    ws.onmessage = (event) => {
-      expect(event.data).toBe('Hello, world! from FirstWebSocket')
-      ws.close()
-      expectsFired++
-    }
-    ws.onopen = () => ws.send('Hello, world!')
-    await new Promise((resolve) => (ws.onclose = resolve))
-    expect(expectsFired).toBe(1)
-  })
-
-  test('should verify the message event with params of the instances', async () => {
-    const ws = new WebSocket(`ws://localhost:${randomPort}/websocket2/1/2`)
-    let expectsFired = 0
-    ws.onmessage = (event) => {
-      expect(event.data).toBe(
-        'WebSocket opened from SecondWebSocket with params {"id2":"2","id":"1"}'
-      )
-      ws.close()
-      expectsFired++
-    }
-    ws.onopen = () => ws.send('Hello, world!')
-    await new Promise((resolve) => (ws.onclose = resolve))
-    expect(expectsFired).toBe(1)
-  })
-
-  test('should verify the close event of the instances', async () => {
-    const ws = new WebSocket(`ws://localhost:${randomPort}/websocket2/1/2`)
-    let expectsFired = 0
-    ws.onclose = (event) => {
-      expect(event.code).toBe(1000)
-      expect(event.reason).toBe(
-        'WebSocket closed from SecondWebSocket with code 1000 and message Goodbye, world! and params {"id":"1","id2":"2"}'
-      )
-      expectsFired++
-    }
-    ws.onopen = () => {
-      ws.send('Close me, please!')
-    }
-    await new Promise((resolve) => (ws.onclose = resolve))
-    expect(expectsFired).toBe(0)
-  })
-})
+    test('should verify the close event of the instances', async () => {
+        const websocket = new WebSocket(`ws://localhost:${randomPort}/websocket2/1/2`);
+        let expectsFired = 0;
+        websocket.onclose = (event) => {
+            expect(event.code).toBe(1000);
+            expect(event.reason).toBe('WebSocket closed from SecondWebSocket with code 1000 and message Goodbye, world! and params {"id":"1","id2":"2"}');
+            expectsFired++;
+        };
+        websocket.onopen = () => {
+            websocket.send('Close me, please!');
+        };
+        await new Promise((resolve) => { websocket.onclose = resolve; });
+        expect(expectsFired).toBe(0);
+    });
+});
