1b505afde8054c9aa8f95793d3485ed80648aa4c
Brian Ferri <brian.ferri@dotenv.it>
2024-03-26 09:23:35 +0100

    Update package version and export fileRouter from server

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,4 +3,5 @@ import Futen from './server';
 export * from './decorators/http';
 export * from './decorators/websocket';
 export * from './server';
+export { routesFrom } from './server/fileRouter';
 export default Futen;
