37d2f4909c33d08ffe0db068e8881edb4701bdb3
Brian Ferri <brian.ferri@dotenv.it>
2024-06-15 13:33:00 +0200

    chore: examples

--- examples/fileRoute.ts
+++ examples/fileRoute.ts
@@ -1,11 +1,20 @@
 import { middleware } from '../dist/index.mjs';
+import type { FutenHTTPRoute } from '../dist/index.mjs';
 
 @middleware((request) => {
     request.headers.set('x-middleware', 'true');
     return request;
 })
-export default class FileRoute {
-    public get(request: Request): Response {
+// The route here is the path of the file relative to where it's being called from
+// index.ts -> `Futen(routesFrom(routes))`
+// routes/
+//     fileRoute.ts -> contains the `FileRoute` class
+//
+export default class FileRoute implements FutenHTTPRoute<'/fileRoute'> {
+    public get(
+        request: Request
+    ): Response {
+        console.log(request.headers.get('x-middleware'));
         return Response.json(
             { hello: 'world' },
             { headers: request.headers }
